decff7fe5250227ac9ae6ef6de8627a4f50b8a38
Kabol Amine <amine.kabol@gmail.com>
2023-08-24 16:52:19 +0200

    mise en place de la visialisation avant css et fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import loader from "./assets/loader.svg"
+
 function App() {
- 
+
   return (
 
       <main>
-      
+        <div className="loader-container">
+          <img src={loader} alt="Loading icon" />
+        </div>
+        <p className="city-name">Paris</p>
+        <p className="contry-name">France</p>
+        <p className="temperature">20°</p>
+        <div className="info-icon-container">
+          <img src="/icons/01d.svg" alt="weather icon" />
+        </div>
       </main>
-  
+
   );
 }
 
