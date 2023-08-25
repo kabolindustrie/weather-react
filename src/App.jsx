9b274ed3a7b01f3df936c7082a0a0648d69e703a
Kabol Amine <amine.kabol@gmail.com>
2023-08-25 17:21:08 +0200

    gestion des erreurs possible avec la methode catch et pour les erreurs servers/clients jutilise !response.ok throw new Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import loader from "./assets/loader.svg"
 import "./App.css"
+import browser from "./assets/browser.svg"
 const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
 
 function App() {
 
   const [weatherData, setWeatherData] = useState(null)
+  const [errorInfo, setErrorInfo] = useState(null)
 
   useEffect(() => {
-    fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
+    fetch(`http://api.airvisual.com/v2/nearest_ciy?key=${APIKEY}`)
       .then(response => {
         console.log(response);
+        // pour les erreurs 400-499 (erreur clients) et erreur 500-599 (erreur serveur)
+        // la methode catch ne les "catch" pas
+        if (!response.ok) throw new Error (`Error ${response.status}, ${response.statusText}`)
         return response.json();
       })
       .then(responseData => {
@@ -23,22 +28,34 @@ function App() {
 
         })
       })
+      .catch(err => {
+        setErrorInfo(err.message)
+      })
   }, [])
 
   return (
 
     <main>
-      <div className={`loader-container ${!weatherData && "active"}`}>
+      <div className={`loader-container ${(!weatherData && !errorInfo) && "active"}`}>
         <img src={loader} alt="Loading icon" />
       </div>
       {weatherData && (
         <>
           <p className="city-name">{weatherData.city}</p>
           <p className="contry-name">{weatherData.contry}</p>
-          <p className="temperature">{weatherData.temperature}</p>
+          <p className="temperature">{weatherData.temperature}°</p>
           <div className="info-icon-container">
             <img src={`/icons/${weatherData.iconId}.svg`} className="info-icon" alt="weather icon" />
           </div>
+        </>
+      )}
+
+      {(errorInfo && !weatherData) && (
+        <>
+          <p className="error-information">
+            {errorInfo}
+          </p>
+          <img src={browser} alt="" />
         </>
       )}
     </main>
